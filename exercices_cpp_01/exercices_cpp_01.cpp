1b458cdae28fdad36e7db00b14002ea5d7230c2f
NetFoley <netfoley@gmail.com>
2020-10-07 10:57:39 +0200

    nom prenom

--- exercices_cpp_01/exercices_cpp_01.cpp
+++ exercices_cpp_01/exercices_cpp_01.cpp
@@ -1,5 +1,5 @@
 // exercices_cpp_01.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Rendu de Valentin Fonderflick
 
 #include <iostream>
 #include "cPoint.h"
